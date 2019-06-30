7165e8bb0a3e73ddba5f4e5804c7d6e109afb01d
Stephen Greer <info@stephengreer.me>
2019-06-29 18:05:50 -0700

    Format files

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,15 +2,14 @@ import React from "react"
 
 import Layout from "../components/Layout/Layout"
 import SEO from "../components/Seo"
-import styles from "./styles/404.module.scss";
+import styles from "./styles/404.module.scss"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <div className={styles.container}>
-
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </div>
   </Layout>
 )
